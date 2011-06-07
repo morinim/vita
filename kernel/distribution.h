714f2517812c9476ad970027d4c9e28e0075a5d9
Manlio Morini <morinim@gmail.com>
2011-06-07 18:30:41 +0200

    distribution: GPL disclaimer and Google C++ Style Guide related adaptations.

--- kernel/distribution.h
+++ kernel/distribution.h
@@ -2,25 +2,40 @@
  *
  *  \file distribution.h
  *
- *  \author Manlio Morini
- *  \date 2011/04/20
+ *  Copyright 2011 EOS di Manlio Morini.
  *
- *  This file is part of VITA
+ *  This file is part of VITA.
+ *  
+ *  VITA is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  VITA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along 
+ *  with VITA. If not, see <http://www.gnu.org/licenses/>. 
  *
  */
-  
+
 #if !defined(DISTRIBUTION_H)
 #define      DISTRIBUTION_H
 
 #include <cmath>
 #include <map>
 
-#include "vita.h"
-#include "fitness.h"
+#include "kernel/vita.h"
+#include "kernel/fitness.h"
 
 namespace vita
 {
-
+  ///
+  /// \a distribution \c class simplify the calculation of statistics regarding
+  /// a sequence (mean, variance, standard deviation, entropy, min and max).
+  ///
   template<class T>
   class distribution
   {
@@ -37,12 +52,12 @@ namespace vita
     bool check() const;
 
     boost::uint64_t count;
-  
+
     T     mean;
     T variance;
     T      min;
     T      max;
-    
+
   private:
     void update_variance(T);
 
@@ -63,8 +78,7 @@ namespace vita
   /// Resets gathered statics.
   ///
   template<class T>
-  void
-  distribution<T>::clear()
+  void distribution<T>::clear()
   {
     count = 0;
 
@@ -79,8 +93,7 @@ namespace vita
   /// Gathers a new fitness for the statistics.
   ///
   template<class T>
-  void
-  distribution<T>::add(T val)
+  void distribution<T>::add(T val)
   {
     if (!count)
       min = max = val;
@@ -93,16 +106,17 @@ namespace vita
     ++freq[val];
 
     update_variance(val);
-  }    
+  }
 
   ///
   /// \return the entropy of the distribution.
   ///
   /// \fH(X)=-\sum_{i=1}^n p(x_i) \dot log_b(p(x_i))
+  /// We use an offline algorithm
+  /// (<http://en.wikipedia.org/wiki/Online_algorithm>).
   ///
   template<class T>
-  double
-  distribution<T>::entropy() const
+  double distribution<T>::entropy() const
   {
     const double c(1.0/std::log(2.0));
 
@@ -111,7 +125,7 @@ namespace vita
          j != freq.end();
          ++j)
     {
-      const double p(double(j->second) / count);
+      const double p(static_cast<double>(j->second) / count);
       h -= p * std::log(p) * c;
     }
 
@@ -123,28 +137,28 @@ namespace vita
   ///
   /// Calculate running variance and cumulative average fitness. The
   /// algorithm used is due to Knuth (Donald E. Knuth - The Art of Computer
-  /// Programming, volume 2: Seminumerical Algorithms, 3rd edn., p. 232. 
-  /// Addison-Wesley)
+  /// Programming, volume 2: Seminumerical Algorithms, 3rd edn., p. 232.
+  /// Addison-Wesley).
+  /// This is an online algorithm
+  /// (<http://en.wikipedia.org/wiki/Online_algorithm>).
   ///
   template<class T>
-  void
-  distribution<T>::update_variance(T val)
-  {   
+  void distribution<T>::update_variance(T val)
+  {
     delta = val - mean;
     mean += delta/count;
-      
+
     // This expression uses the new value of mean.
     m2 += delta * (val-mean);
 
     variance = m2 / count;
-  }    
+  }
 
   ///
   /// \return The standard deviation of the distribution.
   ///
   template<class T>
-  T
-  distribution<T>::standard_deviation() const
+  T distribution<T>::standard_deviation() const
   {
     return std::sqrt(variance);
   }
@@ -153,15 +167,13 @@ namespace vita
   /// \return true if the object passes the internal consistency check.
   ///
   template<class T>
-  bool
-  distribution<T>::check() const
+  bool distribution<T>::check() const
   {
     return
       (is_bad(min) || is_bad(mean) || min <= mean+float_epsilon) &&
       (is_bad(max) || is_bad(mean) || mean <= max+float_epsilon) &&
       (isnan(variance) || 0.0 <= variance+float_epsilon);
   }
-
 }  // Namespace vita
 
-#endif // DISTRIBUTION_H
+#endif  // DISTRIBUTION_H
