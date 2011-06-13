f06a30a7dc67d22d563ec9ec1e2d6dda0723e22f
Manlio Morini <morinim@gmail.com>
2011-06-13 17:12:59 +0200

    interpreter: GPL disclaimer and Google C++ Style Guide related adaptations.

--- kernel/interpreter.cc
+++ kernel/interpreter.cc
@@ -2,21 +2,32 @@
  *
  *  \file interpreter.cc
  *
- *  \author Manlio Morini
- *  \date 2011/02/10
+ *  Copyright (c) 2011 EOS di Manlio Morini.
  *
- *  This file is part of VITA
+ *  This file is part of VITA.
+ *
+ *  VITA is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  VITA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with VITA. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-#include "interpreter.h"
-#include "adf.h"
-#include "individual.h"
-#include "symbol.h"
+#include "kernel/interpreter.h"
+#include "kernel/adf.h"
+#include "kernel/individual.h"
+#include "kernel/symbol.h"
 
 namespace vita
 {
-
   ///
   /// \param[in] ind individual whose value we are interested in,
   /// \param[in] ctx context in which we calculate the output value (used for
@@ -24,40 +35,38 @@ namespace vita
   ///
   interpreter::interpreter(const individual &ind,
                            interpreter *const ctx)
-    : _ip(ind.best_), _context(ctx), _ind(ind),
-      _cache(ind.size()),
-      _context_cache(ctx ? ctx->_ind.size() : 0)
+    : ip_(ind.best_), context_(ctx), ind_(ind),
+      cache_(ind.size()),
+      context_cache_(ctx ? ctx->ind_.size() : 0)
   {
   }
 
   ///
   /// \return the output value of \c this \a individual.
   ///
-  boost::any
-  interpreter::run()
+  boost::any interpreter::run()
   {
-    for (unsigned i(0); i < _cache.size(); ++i)
+    for (unsigned i(0); i < cache_.size(); ++i)
     {
-      _cache[i].empty = true;
-      _cache[i].value = boost::any();
+      cache_[i].empty = true;
+      cache_[i].value = boost::any();
     }
 
-    // Probably the _context_cache vector will be deleted (it was introduced 
-    // before the _cache vector).
-    for (unsigned i(0); i < _context_cache.size(); ++i)
-      _context_cache[i] = boost::any();
+    // Probably the context_cache_ vector will be deleted (it was introduced
+    // before the cache_ vector).
+    for (unsigned i(0); i < context_cache_.size(); ++i)
+      context_cache_[i] = boost::any();
 
-    _ip = _ind.best_;
-    return _ind.code_[_ip].sym->eval(this);
+    ip_ = ind_.best_;
+    return ind_.code_[ip_].sym->eval(this);
   }
 
   ///
   /// \return the output value of the current terminal  symbol.
   ///
-  boost::any
-  interpreter::eval()
+  boost::any interpreter::eval()
   {
-    const gene &g(_ind.code_[_ip]);
+    const gene &g(ind_.code_[ip_]);
 
     assert(g.sym->parametric());
     return g.par;
@@ -67,76 +76,72 @@ namespace vita
   /// \param[in] i i-th argument of the current function.
   /// \return the value of the i-th argument of the current function.
   ///
-  boost::any
-  interpreter::eval(unsigned i)
+  boost::any interpreter::eval(unsigned i)
   {
-    const gene &g(_ind.code_[_ip]);
+    const gene &g(ind_.code_[ip_]);
 
     assert(i < g.sym->arity());
 
     const locus_t locus(g.args[i]);
-    if (_cache[locus].empty)
+    if (cache_[locus].empty)
     {
-      const unsigned backup(_ip);
-      _ip = locus;
-      assert (_ip > backup);
-      const boost::any ret(_ind.code_[_ip].sym->eval(this));
-      _ip = backup;
+      const unsigned backup(ip_);
+      ip_ = locus;
+      assert(ip_ > backup);
+      const boost::any ret(ind_.code_[ip_].sym->eval(this));
+      ip_ = backup;
 
-      _cache[locus].empty = false;
-      _cache[locus].value = ret;
+      cache_[locus].empty = false;
+      cache_[locus].value = ret;
     }
 #if !defined(NDEBUG)
     else
     {
-      const unsigned backup(_ip); 
-      _ip = locus;
-      assert (_ip > backup);
-      const boost::any ret(_ind._code[_ip].sym->eval(this));
-      _ip = backup;
+      const unsigned backup(ip_);
+      ip_ = locus;
+      assert(ip_ > backup);
+      const boost::any ret(ind_.code_[ip_].sym->eval(this));
+      ip_ = backup;
       if (ret.type() == typeid(int))
-	assert(boost::any_cast<int>(ret) == 
-	       boost::any_cast<int>(_cache[locus].value));
+        assert(boost::any_cast<int>(ret) ==
+               boost::any_cast<int>(cache_[locus].value));
       else if (ret.type() == typeid(double))
-	assert(boost::any_cast<double>(ret) == 
-	       boost::any_cast<double>(_cache[locus].value));
+        assert(boost::any_cast<double>(ret) ==
+               boost::any_cast<double>(cache_[locus].value));
     }
 #endif
 
-    assert(!_cache[locus].empty);
-    return _cache[locus].value;
+    assert(!cache_[locus].empty);
+    return cache_[locus].value;
 
-    //return _ind._code[g.args[i]].sym->eval(interpreter(_ind,_context,
-    //                                                   g.args[i]));
+    // return ind_.code_[g.args[i]].sym->eval(interpreter(ind_,context_,
+    //                                                    g.args[i]));
   }
 
   ///
   /// \param[in] i-th argument of the current ADF,
   /// \return the value of the i-th argument of the curren ADF function.
   ///
-  boost::any
-  interpreter::eval_adf_arg(unsigned i)
+  boost::any interpreter::eval_adf_arg(unsigned i)
   {
-    const gene context_g(_context->_ind.code_[_context->_ip]);
+    const gene context_g(context_->ind_.code_[context_->ip_]);
 
-    assert( _context && _context->check() && i < gene_args && 
-            dynamic_cast<const adf_n *>(context_g.sym) );
+    assert(context_ && context_->check() && i < gene_args &&
+           dynamic_cast<const adf_n *>(context_g.sym));
 
-    if (_context_cache[context_g.args[i]].empty())
-      _context_cache[context_g.args[i]] = _context->eval(i);
+    if (context_cache_[context_g.args[i]].empty())
+      context_cache_[context_g.args[i]] = context_->eval(i);
 
-    return _context_cache[context_g.args[i]];
+    return context_cache_[context_g.args[i]];
   }
 
   ///
-  /// \return true if the object passes the internal consistency check.
+  /// \return \c true if the object passes the internal consistency check.
   ///
-  bool
-  interpreter::check() const
+  bool interpreter::check() const
   {
     return
-      _ip < _ind.code_.size() &&
-      (!_context || _context->check());
+      ip_ < ind_.code_.size() &&
+      (!context_ || context_->check());
   }
-
 }  // Namespace vita
--- kernel/interpreter.h
+++ kernel/interpreter.h
@@ -2,31 +2,38 @@
  *
  *  \file interpreter.h
  *
- *  \author Manlio Morini
- *  \date 2011/02/10
+ *  Copyright (c) 2011 EOS di Manlio Morini.
  *
- *  This file is part of VITA
+ *  This file is part of VITA.
+ *
+ *  VITA is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  VITA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with VITA. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-  
+
 #if !defined(INTERPRETER_H)
 #define      INTERPRETER_H
 
-#include <vector>
-
 #include <boost/any.hpp>
 
-#include "vita.h"
+#include <vector>
+
+#include "kernel/vita.h"
 
 namespace vita
 {
-
   class individual;
 
-  ///
-  /// \example example5.cc
-  /// Output value calculation for an individual.
-  ///
   class interpreter
   {
   public:
@@ -41,12 +48,12 @@ namespace vita
     bool check() const;
 
   private:
-    /// Instruction pointer.
-    unsigned _ip;
+    // Instruction pointer.
+    unsigned ip_;
 
-    interpreter *const _context;
+    interpreter *const context_;
 
-    const individual &_ind;
+    const individual &ind_;
 
     struct cache_elem
     {
@@ -54,10 +61,14 @@ namespace vita
       boost::any value;
     };
 
-    mutable std::vector<cache_elem> _cache;
-    mutable std::vector<boost::any> _context_cache;
+    mutable std::vector<cache_elem> cache_;
+    mutable std::vector<boost::any> context_cache_;
   };
-    
+
+  ///
+  /// \example example5.cc
+  /// Output value calculation for an individual.
+  ///
 }  // namespace vita
 
 #endif  // INTERPRETER_H
