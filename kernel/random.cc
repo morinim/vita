fe40cb5871c1b00de72df6fb3ceec06cfb06b7b1
Manlio Morini <morinim@gmail.com>
2011-06-15 16:53:41 +0200

    random: GPL disclaimer and Google C++ Style Guide related adaptations.

--- kernel/random.cc
+++ kernel/random.cc
@@ -2,40 +2,57 @@
  *
  *  \file random.h
  *
- *  \author Manlio Morini
- *  \date 2009/09/14
+ *  Copyright (c) 2011 EOS di Manlio Morini.
  *
- *  This file is part of VITA
+ *  This file is part of VITA.
+ *
+ *  VITA is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  VITA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with VITA. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
-#include "random.h"
+#include "kernel/random.h"
 
 namespace vita
 {
-
-  random::base_gen random::_rng(28071973u);
+  random::base_gen random::rng_(28071973u);
 
   ///
-  /// \param[in] base 
+  /// \param[in] s the seed for the random number generator.
+  ///
+  void random::seed(unsigned s)
+  {
+    rng_.seed(s);
+  }
+
+  ///
+  /// \param[in] base
   /// \param[in] width
   /// \param[in] n
   /// \return the index of the choosen individual.
   ///
-  unsigned
-  random::ring(unsigned base, unsigned width, unsigned n)
+  unsigned random::ring(unsigned base, unsigned width, unsigned n)
   {
     assert(width && n>1);
 
     if (base >= n)
-      return random::between<unsigned>(0,n);
+      return random::between<unsigned>(0, n);
 
     if (width > n)
       width = n;
 
     const unsigned offset(n+base - width/2);
-    
-    return (offset + random::between<unsigned>(0,width)) % n;
-  }
 
+    return (offset + random::between<unsigned>(0, width)) % n;
+  }
 }  // Namespace vita
--- kernel/random.h
+++ kernel/random.h
@@ -2,69 +2,69 @@
  *
  *  \file random.h
  *
- *  \author Manlio Morini
- *  \date 2009/09/14
+ *  Copyright (c) 2011 EOS di Manlio Morini.
  *
- *  This file is part of VITA
+ *  This file is part of VITA.
+ *
+ *  VITA is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any later
+ *  version.
+ *
+ *  VITA is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with VITA. If not, see <http://www.gnu.org/licenses/>.
  *
  */
-  
+
 #if !defined(RANDOM_H)
 #define      RANDOM_H
 
-#include <cstdlib>
-
 #include <boost/random.hpp>
 
-#include "vita.h"
+#include <cstdlib>
+
+#include "kernel/vita.h"
 
 namespace vita
 {
-
   class random
   {
   public:
-    template<class T> static T between(T,T);
+    template<class T> static T between(T, T);
 
     static unsigned ring(unsigned, unsigned, unsigned);
 
     static bool boolean(double);
     static bool boolean();
 
-    static void seed(unsigned long);
+    static void seed(unsigned);
 
   private:
     typedef boost::mt19937 base_gen;
 
-    static base_gen _rng;
+    static base_gen rng_;
   };
 
   ///
-  /// \param[in] s
-  ///
-  inline
-  void
-  random::seed(unsigned long s)
-  {
-    _rng.seed(s);
-  }
-
-  ///
-  /// \param[in] min
-  /// \param[in] sup
-  /// \return 
+  /// \param[in] min minimum random number.
+  /// \param[in] sup upper bound.
+  /// \return a random number in the [min;sup[ range.
   ///
   template<>
   inline
-  double
-  random::between(double min, double sup)
+  double random::between(double min, double sup)
   {
     assert(min < sup);
-    
+
     typedef boost::uniform_real<> dist_t;
     typedef boost::variate_generator<base_gen &, dist_t> gen_t;
 
-    gen_t die(_rng,dist_t(min,sup));
+    gen_t die(rng_, dist_t(min, sup));
     const double ret(die());
     assert(min <= ret && ret < sup);
 
@@ -73,15 +73,14 @@ namespace vita
 
   template<class T>
   inline
-  T
-  random::between(T min, T sup)
+  T random::between(T min, T sup)
   {
     assert(min < sup);
-    
+
     typedef boost::uniform_int<> dist_t;
     typedef boost::variate_generator<base_gen &, dist_t> gen_t;
 
-    gen_t die(_rng,dist_t(min,sup-1));
+    gen_t die(rng_, dist_t(min, sup-1));
     const T ret(die());
     assert(min <= ret && ret < sup);
 
@@ -89,28 +88,25 @@ namespace vita
   }
 
   ///
-  /// \param[in] p
-  /// \return
+  /// \param[in] p a probability ([0;1] range).
+  /// \return \c true \a p% times.
   ///
   inline
-  bool
-  random::boolean(double p)
+  bool random::boolean(double p)
   {
     assert(0 <= p && p <= 1);
 
-    return random::between<double>(0,1) < p;
+    return random::between<double>(0, 1) < p;
   }
 
   ///
-  /// \return
+  /// \return \c true 50% times.
   ///
   inline
-  bool
-  random::boolean()
+  bool random::boolean()
   {
-    return random::between<unsigned>(0,2) != 0;
+    return random::between<unsigned>(0, 2) != 0;
   }
-
 }  // namespace vita
 
 #endif  // RANDOM_H
