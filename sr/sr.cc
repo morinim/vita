02202a63694281f162d953be32d05d7c6644b92c
Manlio Morini <morini@eosdev.it>
2013-05-19 13:59:23 +0200

    Fixed compilation error with minGW

--- sr/sr.cc
+++ sr/sr.cc
@@ -911,8 +911,11 @@ int main(int argc, char *const argv[])
     ui::go();
   else
   {
-    po::options_description interface("Interface");
-    interface.add_options()
+    // Do not change the name of the variable:
+    //   po::options_description interface("Interface");
+    // don't compile with mingw.
+    po::options_description interf("Interface");
+    interf.add_options()
       ("go", po::value<bool>()->zero_tokens()->notifier(&ui::go),
        "let's go!")
       ("environment",
@@ -920,7 +923,7 @@ int main(int argc, char *const argv[])
        "show the environment")
       ("exit", po::value<bool>()->zero_tokens()->notifier(&ui::exit),
        "quit the program");
-    ui::cmdl_opt.add(interface);
+    ui::cmdl_opt.add(interf);
 
     boost::cli::command_line_interpreter cli(ui::cmdl_opt, "> ");
     cli.interpret(std::cin);
