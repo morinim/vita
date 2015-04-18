91ab88573b180ed826b1ad96c83013d03929b38a
Manlio Morini <morinim@gmail.com>
2015-04-18 15:45:21 +0200

    [REF] Removing namespace indentation

--- src/kernel/category_set.h
+++ src/kernel/category_set.h
@@ -2,7 +2,7 @@
  *  \file
  *  \remark This file is part of VITA.
  *
- *  \copyright Copyright (C) 2014 EOS di Manlio Morini.
+ *  \copyright Copyright (C) 2014-2015 EOS di Manlio Morini.
  *
  *  \license
  *  This Source Code Form is subject to the terms of the Mozilla Public
@@ -21,114 +21,115 @@
 
 namespace vita
 {
-  ///
-  /// \brief Informations about a category of the dataset
-  ///
-  /// For example:
-  ///
-  ///     <attribute type="nominal">
-  ///       <labels>
-  ///         <label>Iris-setosa</label>
-  ///         <label>Iris-versicolor</label>
-  ///         <label>Iris-virginica</label>
-  ///       </labels>
-  ///     </attribute>
-  ///
-  /// is mapped to:
-  /// * {"", d_string, {"Iris-setosa", "Iris-versicolor", "Iris-virginica"}}
-  ///
-  /// while
-  ///
-  ///     <attribute type="numeric" category="A" name="Speed" />
-  ///
-  /// is mapped to:
-  /// * {"A", d_double, {}}
-  ///
-  struct untagged_category
+///
+/// \brief Informations about a category of the dataset
+///
+/// For example:
+///
+///     <attribute type="nominal">
+///       <labels>
+///         <label>Iris-setosa</label>
+///         <label>Iris-versicolor</label>
+///         <label>Iris-virginica</label>
+///       </labels>
+///     </attribute>
+///
+/// is mapped to:
+/// * {"", d_string, {"Iris-setosa", "Iris-versicolor", "Iris-virginica"}}
+///
+/// while
+///
+///     <attribute type="numeric" category="A" name="Speed" />
+///
+/// is mapped to:
+/// * {"A", d_double, {}}
+///
+struct untagged_category
+{
+  std::string             name;
+  domain_t              domain;
+  std::set<std::string> labels;
+};
+
+///
+/// \brief Integrates untagged_category struct with a unique identifier
+///
+struct category : public untagged_category
+{
+  category(category_t t, const untagged_category &uc)
+    : untagged_category(uc), tag(t)
   {
-    std::string             name;
-    domain_t              domain;
-    std::set<std::string> labels;
-  };
+  }
 
-  ///
-  /// \brief Integrates untagged_category struct with a unique identifier
-  ///
-  struct category : public untagged_category
-  {
-    category(category_t t, const untagged_category &uc)
-      : untagged_category(uc), tag(t)
-    {
-    }
+  explicit operator bool() const;
 
-    explicit operator bool() const;
+  category_t tag;
 
-    category_t tag;
+  /// A special value used for missing answers.
+  static const category null;
+};  // struct category
 
-    /// A special value used for missing answers.
-    static const category null;
-  };  // struct category
+std::ostream &operator<<(std::ostream &, const category &);
 
-  std::ostream &operator<<(std::ostream &, const category &);
+///
+/// \brief The set of categories used in a specific problem
+///
+class category_set
+{
+public:
+  class const_iterator;
 
-  ///
-  /// \brief The set of categories used in a specific problem
-  ///
-  class category_set
-  {
-  public:
-    class const_iterator;
+  category_set();
 
-    category_set();
+  const_iterator begin() const;
+  const_iterator end() const;
+  unsigned size() const;
 
-    const_iterator begin() const;
-    const_iterator end() const;
-    unsigned size() const;
+  category find(const std::string &) const;
+  category find(category_t) const;
 
-    category find(const std::string &) const;
-    category find(category_t) const;
+  category_t insert(const untagged_category &);
 
-    category_t insert(const untagged_category &);
+  void add_label(category_t, const std::string &);
+  void swap(category_t, category_t);
 
-    void add_label(category_t, const std::string &);
-    void swap(category_t, category_t);
+private:
+  std::vector<untagged_category> categories_;
 
-  private:
-    std::vector<untagged_category> categories_;
+  // Initially we had a std::map from category name to category tag used
+  // to speed up the find(std::string) method.
+  // Now the implementation is a bit slower but simpler and more robust.
+};  // class category_set
 
-    // Initially we had a std::map from category name to category tag used
-    // to speed up the find(std::string) method.
-    // Now the implementation is a bit slower but simpler and more robust.
-  };  // class category_set
+///
+/// \brief Forward iterator to read the elements of a category_set
+///
+class category_set::const_iterator
+{
+private:  // Private data members
+  using const_iter = std::vector<untagged_category>::const_iterator;
 
-  ///
-  /// \brief Forward iterator to read the elements of a category_set
-  ///
-  class category_set::const_iterator
-  {
-  private:  // Private data members
-    using const_iter = std::vector<untagged_category>::const_iterator;
+  const_iter begin_;
+  const_iter ptr_;
 
-    const_iter begin_;
-    const_iter ptr_;
+public:
+  using iterator_category = std::forward_iterator_tag;
+  using difference_type = std::ptrdiff_t;
+  using value_type = category;
+  using pointer = value_type *;
+  using reference = value_type &;
 
-  public:
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type = std::ptrdiff_t;
-    using value_type = category;
-    using pointer = value_type *;
-    using reference = value_type &;
+  const_iterator(const_iter b, const_iter c) : begin_(b), ptr_(c) {}
 
-    const_iterator(const_iter b, const_iter c) : begin_(b), ptr_(c) {}
+  const_iterator operator++() { ++ptr_; return *this; }
+  value_type operator*()
+  { return category(static_cast<category_t>(ptr_ - begin_), *ptr_); }
 
-    const_iterator operator++() { ++ptr_; return *this; }
-    value_type operator*()
-    { return category(static_cast<category_t>(ptr_ - begin_), *ptr_); }
+  bool operator==(const const_iterator &rhs) const
+  { return ptr_ == rhs.ptr_; }
+  bool operator!=(const const_iterator &rhs) const
+  { return ptr_ != rhs.ptr_; }
+};  // class category_set::const_iterator
 
-    bool operator==(const const_iterator &rhs) const
-    { return ptr_ == rhs.ptr_; }
-    bool operator!=(const const_iterator &rhs) const
-    { return ptr_ != rhs.ptr_; }
-  };  // class category_set::const_iterator
 }  // namespace vita
 #endif  // Include guard
