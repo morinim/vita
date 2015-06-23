afe886213de31b08f506430db3f56024e642e041
Manlio Morini <morinim@gmail.com>
2015-06-23 10:30:13 +0200

    [FMT] Removing namespace indentation

--- src/kernel/evolution.h
+++ src/kernel/evolution.h
@@ -24,54 +24,52 @@
 
 namespace vita
 {
-  ///
-  /// \brief Progressively evolves a population of programs over a series of
-  ///        generations
-  ///
-  /// \tparam T type of individual
-  /// \tparam ES evolution strategy in use
-  ///
-  /// The evolutionary search uses the Darwinian principle of natural selection
-  /// (survival of the fittest) and analogs of various naturally occurring
-  /// operations, including crossover (sexual recombination), mutation...
-  ///
-  template<class T, template<class> class ES>
-  class evolution
-  {
-  public:
-    evolution(const environment &, evaluator<T> &,
-              std::function<bool (const summary<T> &)> = nullptr,
-              std::function<void (unsigned)> = nullptr);
+///
+/// \brief Progressively evolves a population of programs over a series of
+///        generations
+///
+/// \tparam T type of individual
+/// \tparam ES evolution strategy in use
+///
+/// The evolutionary search uses the Darwinian principle of natural selection
+/// (survival of the fittest) and analogs of various naturally occurring
+/// operations, including crossover (sexual recombination), mutation...
+///
+template<class T, template<class> class ES>
+class evolution
+{
+public:
+  evolution(const environment &, evaluator<T> &,
+            std::function<bool (const summary<T> &)> = nullptr,
+            std::function<void (unsigned)> = nullptr);
 
-    const summary<T> &run(unsigned);
+  const summary<T> &run(unsigned);
 
-    bool debug(bool) const;
+  bool debug(bool) const;
 
-  private:  // Private support methods
-    const environment &env() const { return pop_.env(); }
-    analyzer<T> get_stats() const;
-    void log(unsigned) const;
-    void print_progress(unsigned, unsigned, bool) const;
-    bool stop_condition(const summary<T> &) const;
+private:  // Private support methods
+  const environment &env() const { return pop_.env(); }
+  analyzer<T> get_stats() const;
+  void log(unsigned) const;
+  void print_progress(unsigned, unsigned, bool) const;
+  bool stop_condition(const summary<T> &) const;
 
-  private:  // Private data members
-    vita::population<T> pop_;
-    evaluator<T>       &eva_;
-    summary<T>        stats_;
-    ES<T>                es_;
+private:  // Private data members
+  vita::population<T> pop_;
+  evaluator<T>       &eva_;
+  summary<T>        stats_;
+  ES<T>                es_;
 
-    std::function<bool (const summary<T> &)>
-    external_stop_condition_;
-
-    std::function<void (unsigned)> shake_data_;
-  };
+  std::function<bool (const summary<T> &)> external_stop_condition_;
+  std::function<void (unsigned)>                        shake_data_;
+};
 
 #include "kernel/evolution.tcc"
 
-  ///
-  /// \example example6.cc
-  /// Simple symbolic regression example (\f$x^2+y^2-z^2\f$).
-  ///
+///
+/// \example example6.cc
+/// Simple symbolic regression example (\f$x^2+y^2-z^2\f$).
+///
 }  // namespace vita
 
 #endif  // Include guard
