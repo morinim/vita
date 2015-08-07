0d6be45c51c187cb0d9b3ed9dc8a340193439ef7
Manlio Morini <morinim@gmail.com>
2015-08-07 09:54:33 +0200

    [DOC] Doxygen related files added to the repository

---
+++ doc/doxygen/doxygen.h
@@ -0,0 +1,49 @@
+/**
+ *  \file
+ *  \remark This file is part of VITA.
+ *
+ *  \copyright Copyright (C) 2013-2015 EOS di Manlio Morini.
+ *
+ *  \license
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ *
+ *  \mainpage VITA v0.9.11
+ *
+ *  \section Introduction
+ *  Welcome to the Vita project.
+ *
+ *  This is the reference guide for the Vita APIs.
+ *
+ *  \section note_sec Notes
+ *  New versions of this program will be available at
+ *  https://bitbucket.org/morinim/vita
+ *
+ *  Please reports any suggestions and/or bugs to:
+ *  <dl>
+ *    <dt>Issue tracking system</dt>
+ *    <dd>https://bitbucket.org/morinim/vita/issues</dd>
+ *    <dt>The project owner</dt>
+ *    <dd>https://bitbucket.org/morinim/</dd>
+ *  </dl>
+ *
+ */
+
+
+/**
+ *  \namespace vita
+ *  The main namespace for the project.
+ *
+ *  \namespace detail
+ *  The contents of this namespace isn't intended for general consumption: it
+ *  contains implementation details, not the interface.
+ *  It is recommended giving details their own file and tucking it away in a
+ *  detail folder.
+ */
+
+// This is a global "project documentation" file that supplies the front page
+// docs for the project, sets up the groups (for use with /ingroup tags) and
+// documents the namespaces all in one place. This allows all the "overviews"
+// to be held in one logical place rather than scattered to the winds.
