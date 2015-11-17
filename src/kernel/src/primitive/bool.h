7be95b9081cb039a0c3149650e1576b7ec9d0459
Manlio Morini <morinim@gmail.com>
2015-11-17 17:27:06 +0100

    [FIX] Various compilation errors for boolean symbols

--- src/kernel/src/primitive/bool.h
+++ src/kernel/src/primitive/bool.h
@@ -2,7 +2,7 @@
  *  \file
  *  \remark This file is part of VITA.
  *
- *  \copyright Copyright (C) 2011-2014 EOS di Manlio Morini.
+ *  \copyright Copyright (C) 2011-2015 EOS di Manlio Morini.
  *
  *  \license
  *  This Source Code Form is subject to the terms of the Mozilla Public
@@ -20,69 +20,78 @@
 #include "kernel/interpreter.h"
 #include "kernel/terminal.h"
 
-namespace vita { namespace boolean
+namespace vita
 {
-  class zero : public terminal
+namespace boolean
+{
+class zero : public terminal
+{
+public:
+  explicit zero(const cvect &c) : terminal("0", c[0])
+  { assert(c.size() == 1); }
+
+  virtual std::string display() const override { return "0"; }
+
+  virtual any eval(core_interpreter *) const override { return any(false); }
+};
+
+class one : public terminal
+{
+public:
+  explicit one(const cvect &c) : terminal("1", c[0])
+  { assert(c.size() == 1); }
+
+  virtual std::string display() const override { return "1"; }
+
+  virtual any eval(core_interpreter *) const override { return any(true); }
+};
+
+class l_and : public function
+{
+public:
+  explicit l_and(const cvect &c) : function("AND", c[0], {c[0], c[0]})
+  { assert(c.size() == 1); }
+
+  virtual bool associative() const override { return true; }
+
+  virtual any eval(core_interpreter *ci) const override
   {
-  public:
-    explicit zero(const cvect &c) : terminal("0", c[0])
-    { assert(c.size() == 1); }
+    auto *const i(static_cast<interpreter<i_mep> *>(ci));
+    return any(any_cast<bool>(i->fetch_arg(0)) &&
+               any_cast<bool>(i->fetch_arg(1)));
+  }
+};
 
-    virtual std::string display() const override { return "0"; }
+class l_not : public function
+{
+public:
+  explicit l_not(const cvect &c) : function("NOT", c[0], {c[0]})
+  { assert(c.size() == 1); }
 
-    virtual any eval(interpreter<i_mep> *) const override { return false; }
-  };
-
-  class one : public terminal
+  virtual any eval(core_interpreter *ci) const override
   {
-  public:
-    explicit one(const cvect &c) : terminal("1", c[0])
-    { assert(c.size() == 1); }
+    auto *const i(static_cast<interpreter<i_mep> *>(ci));
+    return any(!any_cast<bool>(i->fetch_arg(0)));
+  }
+};
 
-    virtual std::string display() const override { return "1"; }
+class l_or : public function
+{
+public:
+  explicit l_or(const cvect &c) : function("OR", c[0], {c[0], c[0]})
+  { assert(c.size() == 1); }
 
-    virtual any eval(interpreter<i_mep> *) const override { return true; }
-  };
+  virtual bool associative() const override { return true; }
 
-  class and : public function
+  virtual any eval(core_interpreter *ci) const override
   {
-  public:
-    explicit and(const cvect &c) : function("AND", c[0], {c[0], c[0]})
-    {
-      assert(c.size() == 1);
-      associative_ = true;
-    }
+    auto *const i(static_cast<interpreter<i_mep> *>(ci));
+    return any(any_cast<bool>(i->fetch_arg(0)) ||
+               any_cast<bool>(i->fetch_arg(1)));
+  }
+};
 
-    virtual any eval(interpreter<i_mep> *i) const override
-    {
-      return any_cast<bool>(i->eval(0)) && any_cast<bool>(i->eval(1));
-    }
-  };
-
-  class not : public function
-  {
-  public:
-    explicit not(const cvect &c) : function("NOT", c[0], {c[0]})
-    { assert(c.size() == 1); }
-
-    virtual any eval(interpreter<i_mep> *i) const override
-    { return !any_cast<bool>(i->eval(0)); }
-  };
-
-  class or : public function
-  {
-  public:
-    explicit or(const cvect &c) : function("OR", c[0], {c[0], c[0]})
-    {
-      assert(c.size() == 1);
-      associative_ = true;
-    }
-
-    virtual any eval(interpreter<i_mep> *i) const override
-    {
-      return any_cast<bool>(i->eval(0)) || any_cast<bool>(i->eval(1));
-    }
-  };
-} }  // namespace vita::boolean
+}  // namespace boolean
+}  // namespace vita
 
 #endif  // Include guard
