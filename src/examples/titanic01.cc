1313b71d2d8343aafc541fe8d048b804755e4b78
Manlio Morini <morinim@gmail.com>
2016-08-09 16:04:39 +0200

    [DOC] References to online documentation

--- src/examples/titanic01.cc
+++ src/examples/titanic01.cc
@@ -1,12 +1,14 @@
 /*
  *  \remark This file is part of VITA.
  *
- *  \copyright Copyright (C) 2013-2015 EOS di Manlio Morini.
+ *  \copyright Copyright (C) 2013-2016 EOS di Manlio Morini.
  *
  *  \license
  *  This Source Code Form is subject to the terms of the Mozilla Public
  *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
  *  You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ *  \see https://bitbucket.org/morinim/vita/wiki/titanic_tutorial
  */
 
 #include <cstdlib>
