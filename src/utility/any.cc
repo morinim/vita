7e5ee7c61ec8bac61b3bee450046a306a941a553
Manlio Morini <morinim@gmail.com>
2017-06-19 17:51:06 +0200

    [FMT] Fixing indentation in any.cc

--- src/utility/any.cc
+++ src/utility/any.cc
@@ -2,7 +2,7 @@
  *  \file
  *  \remark This file is part of VITA.
  *
- *  \copyright Copyright (C) 2013-2016 EOS di Manlio Morini.
+ *  \copyright Copyright (C) 2013-2017 EOS di Manlio Morini.
  *
  *  \license
  *  This Source Code Form is subject to the terms of the Mozilla Public
@@ -16,53 +16,60 @@
 
 namespace vita
 {
-  ///
-  /// \param[in] a value that should be converted to `double`.
-  /// \return the result of the conversion of \a a in a `double`.
-  ///
-  /// This function is useful for:
-  /// * debugging purpose (otherwise comparison of `any` values is complex);
-  /// * symbolic regression and classification task (the value returned by
-  ///   the interpeter will be used in a "numeric way").
-  ///
-  template<>
-  double to<double>(const any &a)
-  {
-    // The pointer form of any_cast uses the nullability of pointers (will
-    // return a null pointer rather than throw if the cast fails).
-    // The alternatives are a.type() == typeid(double)... or try/catch and
-    // both seems inferior.
-    if (auto *p = any_cast<double>(&a))
-      return *p;
+///
+/// Converts an `any` to `double`.
+///
+/// \param[in] a value that should be converted to `double`
+/// \return      the result of the conversion of `a`
+///
+/// This function is useful for:
+/// * debugging purpose (otherwise comparison of `any` values is complex);
+/// * symbolic regression and classification task (the value returned by
+///   the interpeter will be used in a "numeric way").
+///
+/// \remark If the conversion cannot be performed the function returns `0.0`.
+///
+template<>
+double to<double>(const any &a)
+{
+  // The pointer form of any_cast uses the nullability of pointers (will
+  // return a null pointer rather than throw if the cast fails).
+  // The alternatives are a.type() == typeid(double)... or try/catch and
+  // both seems inferior.
+  if (auto *p = any_cast<double>(&a))
+    return *p;
 
-    if (auto *p = any_cast<int>(&a))
-      return static_cast<double>(*p);
+  if (auto *p = any_cast<int>(&a))
+    return static_cast<double>(*p);
 
-    if (auto *p = any_cast<bool>(&a))
-      return static_cast<double>(*p);
+  if (auto *p = any_cast<bool>(&a))
+    return static_cast<double>(*p);
 
-    return 0.0;
-  }
+  return 0.0;
+}
 
-  ///
-  /// \param[in] a value that should be converted to `std::string`.
-  /// \return the result of the conversion of \a a in a string.
-  ///
-  /// This function is useful for debugging purpose (otherwise comparison /
-  /// printing of `any` values is complex).
-  ///
-  template<>
-  std::string to<std::string>(const any &a)
-  {
-    if (auto *p = any_cast<double>(&a))
-      return std::to_string(*p);
+///
+/// Converts an `any` to `std::string`.
+///
+/// \param[in] a value that should be converted to `std::string`
+/// \return      the result of the conversion of `a`
+///
+/// This function is useful for debugging purpose (otherwise comparison /
+/// printing of `any` values is complex).
+///
+template<>
+std::string to<std::string>(const any &a)
+{
+  if (auto *p = any_cast<double>(&a))
+    return std::to_string(*p);
 
-    if (auto *p = any_cast<int>(&a))
-      return std::to_string(*p);
+  if (auto *p = any_cast<int>(&a))
+    return std::to_string(*p);
 
-    if (auto *p = any_cast<bool>(&a))
-      return std::to_string(*p);
+  if (auto *p = any_cast<bool>(&a))
+    return std::to_string(*p);
 
-    return any_cast<std::string>(a);
-  }
+  return any_cast<std::string>(a);
+}
+
 }  // namespace vita
