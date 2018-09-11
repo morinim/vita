f1ef25e64fa102942ba9373ec0330e0e63a65de4
Manlio Morini <morinim@gmail.com>
2018-09-11 11:41:35 +0200

    [FIX] `symbol` abstract class missing virtual destructor

--- src/kernel/symbol.h
+++ src/kernel/symbol.h
@@ -2,7 +2,7 @@
  *  \file
  *  \remark This file is part of VITA.
  *
- *  \copyright Copyright (C) 2011-2017 EOS di Manlio Morini.
+ *  \copyright Copyright (C) 2011-2018 EOS di Manlio Morini.
  *
  *  \license
  *  This Source Code Form is subject to the terms of the Mozilla Public
@@ -37,6 +37,7 @@ public:
   enum format {c_format, cpp_format, mql_format, python_format, sup_format};
 
   symbol(const std::string &, category_t);
+  virtual ~symbol() = default;
 
   virtual unsigned arity() const = 0;
   virtual bool auto_defined() const;
